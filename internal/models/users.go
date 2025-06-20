2d19ca3360e4eb8cf1d601ec585086cfd5227790
Rodrigo Moran <rmoran@captechconsulting.com>
2025-06-20 12:51:47 -0400

    add assert package and embed templates

--- internal/models/users.go
+++ internal/models/users.go
@@ -70,6 +70,11 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 	return id, nil
 }
 
-func (m *UserModel) Exists(name, email, password string) (bool, error) {
-	return false, nil
+func (m *UserModel) Exists(id int) (bool, error) {
+	var exists bool
+
+	stmt := "SELECT EXISTS(SELECT true from users WHERE id = ?)"
+
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	return exists, err
 }
